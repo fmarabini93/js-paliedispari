359a850dc043111e60603978c410348ad32e6b7c
fmarabini93 <fmarabini@icloud.com>
2021-05-03 15:27:03 +0200

    evenOdd-even-odd-initial-check

--- evenOdd/js/script.js
+++ evenOdd/js/script.js
@@ -5,6 +5,11 @@ function isEven (num) {
       return (num % 2 == 0);
 }
 
+var choice;
+do {
+      choice = prompt ("Choose between 'even' and 'odd'");
+} while (choice !== "even" && choice !== "odd")
+
 var userNum;
 do {
       userNum = parseInt(prompt("Please type a number between 1 and 5 (included)"));
@@ -13,11 +18,17 @@ do {
 var pcNum = rndNum(1, 5);
 var sum = userNum + pcNum;
 var result;
+var response;
 
 if (isEven(sum) == true) {
-      result = "EVEN";
+      result = "even";
 } else {
-      result = "ODD";
+      result = "odd";
+}
+if (choice === result) {
+      response = "You won!";
+} else {
+      response = "You lost!";
 }
 
-document.getElementById("result").innerHTML = "Your number is " + userNum + " , PC number is " + pcNum + " , so the sum is " + sum + " and the result is " + result;
+document.getElementById("result").innerHTML = "Your number is " + userNum + " , PC number is " + pcNum + " , so the sum is " + sum + " and the result is " + result + ". " + response;
